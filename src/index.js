30a8dd5b65ef13c44a7a0814cfe4f1c45b49e407
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-09-14 17:00:33 +0300

    first issue done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@
  * @return {Promise}
  */
 function delayPromise(seconds) {
+    var sec = seconds * 1000;
+    var p = new Promise(function(resolv) {
+        setTimeout(function() {
+            resolv(); 
+        }, sec);
+    });
+    
+    return p;
 }
 
 /**
@@ -17,6 +25,42 @@ function delayPromise(seconds) {
  * @return {Promise<Array<{name: String}>>}
  */
 function loadAndSortTowns() {
+    let prom = new Promise(function(resolve) {
+        let xhr = new XMLHttpRequest();
+
+        // получаем данные от ресурса
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+        xhr.send();
+
+        // после загрузки данных производим необходимые манипуляции
+        xhr.addEventListener('load', function() {
+            let list = xhr.response;
+            let cityArray = [];
+            let sortCityArray = [];
+            
+            // создаем простой массив из названий городов 
+            list.forEach(function(city) {
+                cityArray.push(city.name);
+            });
+
+            // сортируем массив
+            cityArray = cityArray.sort();
+
+            // создаем новый отсортированный массив с объектами городами
+            cityArray.forEach(function(city) {
+                var obj = {
+                    'name': city
+                }
+
+                sortCityArray.push(obj);
+            });
+
+            resolve(sortCityArray);
+        })
+    });
+
+    return prom;
 }
 
 export {
