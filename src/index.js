ab9feefa2fb8f7bb5738e5f94691babfb2de8136
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-09-15 14:01:25 +0300

    delet old files, add dz7 files

--- src/index.js
+++ src/index.js
@@ -1,69 +1,44 @@
-/* ДЗ 6.1 - Асинхронность и работа с сетью */
+/* ДЗ 7.1 - BOM */
 
 /**
- * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ * Функция должна создавать окно с указанным именем и размерами
  *
- * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
- * @return {Promise}
+ * @param {number} name - имя окна
+ * @param {number} width - ширина окна
+ * @param {number} height - высота окна
+ * @return {Window}
  */
-function delayPromise(seconds) {
-    var sec = seconds * 1000;
-    var p = new Promise(function(resolv) {
-        setTimeout(function() {
-            resolv(); 
-        }, sec);
-    });
-    
-    return p;
+function createWindow(name, width, height) {
 }
 
 /**
- * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
- * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- * Элементы полученного массива должны быть отсортированы по имени города
+ * Функция должна закрывать указанное окно
  *
- * @return {Promise<Array<{name: String}>>}
+ * @param {Window} window - окно, размер которого надо изменить
  */
-function loadAndSortTowns() {
-    let prom = new Promise(function(resolve) {
-        let xhr = new XMLHttpRequest();
+function closeWindow(window) {
+}
 
-        // получаем данные от ресурса
-        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
-        xhr.responseType = 'json';
-        xhr.send();
+/**
+ * Функция должна создавать cookie с указанными именем и значением
+ *
+ * @param name - имя
+ * @param value - значение
+ */
+function createCookie(name, value) {
+}
 
-        // после загрузки данных производим необходимые манипуляции
-        xhr.addEventListener('load', function() {
-            let list = xhr.response;
-            let cityArray = [];
-            let sortCityArray = [];
-            
-            // создаем простой массив из названий городов 
-            list.forEach(function(city) {
-                cityArray.push(city.name);
-            });
-
-            // сортируем массив
-            cityArray = cityArray.sort();
-
-            // создаем новый отсортированный массив с объектами городами
-            cityArray.forEach(function(city) {
-                var obj = {
-                    'name': city
-                }
-
-                sortCityArray.push(obj);
-            });
-
-            resolve(sortCityArray);
-        })
-    });
-
-    return prom;
+/**
+ * Функция должна удалять cookie с указанным именем
+ *
+ * @param name - имя
+ */
+function deleteCookie(name) {
 }
 
 export {
-    delayPromise,
-    loadAndSortTowns
+    createWindow,
+    closeWindow,
+    createCookie,
+    deleteCookie
 };
